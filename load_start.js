c9902f9fc52807aaaee543880a8898ed4311817a
Don Pham <wrathofdon@gmail.com>
2018-02-25 16:18:59 -0800

    Update load_start.js

--- load_start.js
+++ load_start.js
@@ -1,9 +1,14 @@
-var rawText = `
+ var rawText = `
 
-===START
+// You can add comments with double slashes
 
-[b]What example would you like to cover?[/b]
+
+===START // The '===' symbol designates a new slide.  'START' is the title.
+[b]What example would you like to cover?[/b] // then you follow up with text.  Limited BB Code supported.
 
 + Immigration -> START_IMMIGRATION
+
+// '+' creates a button. '->' links the button to another slide.
+
 + Using past statements in court -> START_PAST
 `
