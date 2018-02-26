7522f3e81ad1dec34dffa2247f6d60b322979574
Don Pham <wrathofdon@gmail.com>
2018-02-25 16:25:50 -0800

    Update load_start.js

--- load_start.js
+++ load_start.js
@@ -6,6 +6,6 @@
 ===START // The '===' symbol designates a new slide.  'START' is the title.
 [b]What example would you like to cover?[/b] // then you follow up with text.  Limited BB Code supported.
 
-+ Immigration -> START_IMMIGRATION
-+ Using past statements in court -> START_PAST
++ Immigration -> START_IMMIGRATION    // '+' creates a button
++ Using past statements in court -> START_PAST     // '->' links the button to a new slide
 `
